99c7f8884b287422afa80276aa4a8fa1c435f819
dilys zhu <520dilys@gmail.com>
2016-09-01 21:03:54 -0400

    Corrected hw#3. Played around with hw#4. Classwork07.

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,30 @@
+	alert('this is an alert. this is annoying');
+document.write("the most boring function, maybe")
+			document.write('<br/>');
+
+var num1 = 10;
+
+			document.write(num1*10);
+var num3;
+console.log(num1);
+console.log(num3);
+
+function hello () {
+	var greeting = 'hello!';
+	greeting = 'hello, world!';
+
+	alert(greeting);
+}
+
+hello ();
+
+document.getElementById('title').style.color='red';
+
+
+function doSomething (){
+	console.log('this is working');
+}
+
+
+
+doSomething ();
